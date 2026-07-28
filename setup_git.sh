acd9196ab2f37b372f9662418f07e13309eda16d
ipshita <ipshita.1602@gmail.com>
2026-07-28 15:39:25 +0530

    Initial project setup with frontend, admin, and backend

---
+++ setup_git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git init
+git add .
+git commit -m "Initial project setup with frontend, admin, and backend"
+git branch -M main
+git remote add origin https://github.com/Ipshita2884/myblyk.git
+rm setup_git.sh
